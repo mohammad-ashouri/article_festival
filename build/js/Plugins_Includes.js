b8ac37cd44bcbe351a00c48788b18af5bf964eb4
exploiter0253 <exploiter0253@gmail.com>
2023-05-02 10:10:45 +0330

    initial project

---
+++ build/js/Plugins_Includes.js
@@ -0,0 +1,24 @@
+$(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Datemask dd/mm/yyyy
+    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    //Datemask2 mm/dd/yyyy
+    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    //Money Euro
+    $('[data-mask]').inputmask()
+
+    $('.festival_start_date').pDatepicker(
+        {
+            viewMode: 'year/month/day',
+            format: 'YYYY/MM/DD',
+        }
+    );
+    $('.normal-example').pDatepicker(
+        {
+            viewMode: 'year',
+            format: 'YYYY',
+        }
+    );
+})
