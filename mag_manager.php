45c551f8020f823fc25604bb8c28425d020061ba
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-05-05 20:08:55 +0430

    Update NPM package.json + Fix Modal Width

--- mag_manager.php
+++ mag_manager.php
@@ -284,13 +284,13 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                                                 <th>نام و نام خانوادگی*</th>
                                                                 <td>
                                                                     <input type="text"
-                                                                           style="width: 49%; display: inherit"
+                                                                           style="width: 49%; display: inline-block"
                                                                            class="form-control"
                                                                            id="responsible_manager_owner_name"
                                                                            placeholder="نام"
                                                                            name="responsible_manager_owner_name">
                                                                     <input type="text"
-                                                                           style="width: 50%; display: inherit"
+                                                                           style="width: 50%; display: inline-block"
                                                                            class="form-control"
                                                                            id="responsible_manager_owner_family"
                                                                            placeholder="نام خانوادگی"
@@ -370,11 +370,11 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                                                 <th>نام و نام خانوادگی*</th>
                                                                 <td>
                                                                     <input type="text"
-                                                                           style="width: 49%; display: inherit"
+                                                                           style="width: 49%; display: inline-block"
                                                                            class="form-control" id="chief_editor_name"
                                                                            placeholder="نام" name="chief_editor_name">
                                                                     <input type="text"
-                                                                           style="width: 50%; display: inherit"
+                                                                           style="width: 50%; display: inline-block"
                                                                            class="form-control" id="chief_editor_family"
                                                                            placeholder="نام خانوادگی"
                                                                            name="chief_editor_family">
@@ -453,13 +453,13 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                                                 <th>نام و نام خانوادگی*</th>
                                                                 <td>
                                                                     <input type="text"
-                                                                           style="width: 49%; display: inherit"
+                                                                           style="width: 49%; display: inline-block"
                                                                            class="form-control"
                                                                            id="administration_manager_name"
                                                                            placeholder="نام"
                                                                            name="administration_manager_name">
                                                                     <input type="text"
-                                                                           style="width: 50%; display: inherit"
+                                                                           style="width: 50%; display: inline-block"
                                                                            class="form-control"
                                                                            id="administration_manager_family"
                                                                            placeholder="نام خانوادگی"
@@ -620,7 +620,7 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                         <div class="modal fade" id="editModal" tabindex="-1"
                                              aria-labelledby="exampleModalLabel" aria-hidden="true">
                                             <div class="modal-dialog">
-                                                <div class="modal-content">
+                                                <div class="modal-content" style="width: 800px">
                                                     <div class="modal-header">
                                                         <h1 class="modal-title fs-5" id="exampleModalLabel">ویرایش
                                                             اطلاعات نشریه</h1>
@@ -841,13 +841,13 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                                                             <th>نام و نام خانوادگی*</th>
                                                                             <td>
                                                                                 <input type="text"
-                                                                                       style="width: 49%; display: inherit"
+                                                                                       style="width: 49%; display: inline-block"
                                                                                        class="form-control"
                                                                                        id="editedResponsibleManagerOwnerName"
                                                                                        placeholder="نام"
                                                                                        name="responsible_manager_owner_name">
                                                                                 <input type="text"
-                                                                                       style="width: 50%; display: inherit"
+                                                                                       style="width: 49%; display: inline-block"
                                                                                        class="form-control"
                                                                                        id="editedResponsibleManagerOwnerFamily"
                                                                                        placeholder="نام خانوادگی"
@@ -928,11 +928,11 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                                                             <th>نام و نام خانوادگی*</th>
                                                                             <td>
                                                                                 <input type="text"
-                                                                                       style="width: 49%; display: inherit"
+                                                                                       style="width: 49%; display: inline-block"
                                                                                        class="form-control" id="editedChiefEditorName"
                                                                                        placeholder="نام" name="chief_editor_name">
                                                                                 <input type="text"
-                                                                                       style="width: 50%; display: inherit"
+                                                                                       style="width: 50%; display: inline-block"
                                                                                        class="form-control" id="editedChiefEditorFamily"
                                                                                        placeholder="نام خانوادگی"
                                                                                        name="chief_editor_family">
@@ -1012,13 +1012,13 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
                                                                             <th>نام و نام خانوادگی*</th>
                                                                             <td>
                                                                                 <input type="text"
-                                                                                       style="width: 49%; display: inherit"
+                                                                                       style="width: 49%; display: inline-block"
                                                                                        class="form-control"
                                                                                        id="editedAdministrationManagerName"
                                                                                        placeholder="نام"
                                                                                        name="administration_manager_name">
                                                                                 <input type="text"
-                                                                                       style="width: 50%; display: inherit"
+                                                                                       style="width: 50%; display: inline-block"
                                                                                        class="form-control"
                                                                                        id="editedAdministrationManagerFamily"
                                                                                        placeholder="نام خانوادگی"
@@ -1077,9 +1077,9 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
 
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary"
-                                                                data-bs-dismiss="modal">Close
+                                                                data-bs-dismiss="modal">بستن
                                                         </button>
-                                                        <button type="button" class="btn btn-primary">Save changes
+                                                        <button type="button" class="btn btn-primary">ذخیره تغییرات
                                                         </button>
                                                     </div>
                                                 </div>
@@ -1113,65 +1113,8 @@ if ($_SESSION['head'] == 4 or $_SESSION['head'] == 3):
     <script src="build/js/SearchInMagManagerTable.js"></script>
     <script src="build/js/ChangeTabsInMagManager.js"></script>
     <script src="build/js/Mag_Manager_Check_Fields.js"></script>
-    <script>
-        function getInfo(id) {
-            $.ajax({
-                url: "build/ajax/GetMagAllInfo.php",
-                type: "GET",
-                data: {
-                    id: id,
-                    name: ''
-                },
-                success: function (response) {
-                    //Mag_Info
-                    $("#editedName").val(response.name);
-                    $("#editedScienceRank").val(response.science_rank).trigger('change');
-                    $("#editedScientificGroup").val(response.scientific_group.split('/')).trigger('change');
-                    $("#editedInternationalPosition").val(response.international_position.split('/')).trigger('change');
-                    $("#editedMagType").val(response.mag_type).trigger('change');
-                    $("#editedPublicationPeriod").val(response.publication_period).trigger('change');
-                    $("#editedISSN").val(response.ISSN);
-                    $("#editedMagState").val(response.mag_state);
-                    $("#editedMagCity").val(response.mag_city);
-                    $("#editedMagAddress").val(response.mag_address);
-                    $("#editedMagPhone").val(response.mag_phone);
-                    $("#editedMagFax").val(response.mag_fax);
-                    $("#editedMagEmail").val(response.mag_email);
-                    $("#editedWebsite").val(response.mag_website);
-                    $("#editedConcessionaireType").val(response.concessionaire_type).trigger('change');
-                    $("#editedConcessionaire").val(response.concessionaire).trigger('change');
-
-                    //responsible_manager_owner
-                    $("#editedResponsibleManagerOwnerSubject").val(response.responsible_manager_owner_subject).trigger('change');
-                    $("#editedResponsibleManagerOwnerName").val(response.responsible_manager_owner_name);
-                    $("#editedResponsibleManagerOwnerFamily").val(response.responsible_manager_owner_family);
-                    $("#editedResponsibleManagerOwnerDegree").val(response.responsible_manager_owner_degree).trigger('change');
-                    $("#editedResponsibleManagerOwnerPhone").val(response.responsible_manager_owner_phone);
-                    $("#editedResponsibleManagerOwnerMobile").val(response.responsible_manager_owner_mobile);
-                    $("#editedResponsibleManagerOwnerAddress").val(response.responsible_manager_owner_address);
-
-                    //responsible_manager_owner
-                    $("#editedChiefEditorSubject").val(response.chief_editor_subject).trigger('change');
-                    $("#editedChiefEditorName").val(response.chief_editor_name);
-                    $("#editedChiefEditorFamily").val(response.chief_editor_family);
-                    $("#editedChiefEditorDegree").val(response.chief_editor_degree).trigger('change');
-                    $("#editedChiefEditorPhone").val(response.chief_editor_phone);
-                    $("#editedChiefEditorMobile").val(response.chief_editor_mobile);
-                    $("#editedChiefEditorAddress").val(response.chief_editor_address);
-
-                    //administration_manager
-                    $("#editedAdministrationManagerSubject").val(response.administration_manager_subject).trigger('change');
-                    $("#editedAdministrationManagerName").val(response.administration_manager_name);
-                    $("#editedAdministrationManagerFamily").val(response.administration_manager_family);
-                    $("#editedAdministrationManagerDegree").val(response.administration_manager_degree).trigger('change');
-                    $("#editedAdministrationManagerPhone").val(response.administration_manager_phone);
-                    $("#editedAdministrationManagerMobile").val(response.administration_manager_mobile);
-                    $("#editedAdministrationManagerAddress").val(response.administration_manager_address);
-                }
-            });
-        }
-
-    </script>
+    <script src="build/js/getMagInfo.js"></script>
+    <script src="build/js/setUpdatedMagInfo.js"></script>
 <?php
 endif;
 include_once __DIR__ . '/footer.php'; ?>
