c44d3b847ab3fcb891e3c6f569caeb27044b411f
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-08-29 11:05:35 +0430

    Fix Change Article File

--- build/php/inc/EditArticle.php
+++ build/php/inc/EditArticle.php
@@ -13,11 +13,13 @@ if (isset($_POST['UploadArticleFile']) and !empty($_SESSION['id']) and !empty($_
         $encodedString = bin2hex($bytes);
         $folder_name = $encodedString . 'Article' . ' Mag-Ver ' . $articleInfo['mag_version_id'];
         $file_url_tmpname = $_FILES["ArticleFile"]["tmp_name"];
-        mkdir(__DIR__ . "/../../../Files/Mag_Files/" . $folder[0] . '/' . $folder_name);
+        if (!mkdir($concurrentDirectory = __DIR__ . "/../../../Files/Mag_Files/" . $folder[0] . '/' . $folder_name) && !is_dir($concurrentDirectory)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+        }
         move_uploaded_file($file_url_tmpname, __DIR__ . "/../../../Files/Mag_Files/$folder[0]/$folder_name/" . $_FILES["ArticleFile"]["name"]);
         $file_url = $folder[0] . "/$folder_name/" . $_FILES["ArticleFile"]["name"];
         mysqli_query($connection_mag, "update mag_articles set file_url='$file_url',editor='$user',edited_date='$datewithtime' where id='$ArticleID'");
-        header("Location: ../../../catalogs.php?Article_File_Set");
+        header("Location: ../../../article_manager.php?Article_File_Set");
     } else {
         header("Location: ../../../article_manager.php?Article_Not_Founded");
     }
