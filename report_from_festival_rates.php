678880483c394844b3e37ce18d661d41c0e21a96
Mohammad <exploiter0253@gmail.com>
2023-10-21 20:37:23 +0330

    Add group filtering in report_from_festival_rates

--- report_from_festival_rates.php
+++ report_from_festival_rates.php
@@ -30,6 +30,74 @@
                     echo 'مقاله ای پیدا نشد.';
                 } else {
                     ?>
+                    <div style="margin-bottom: 20px; overflow-x: auto">
+                        <label>گروه علمی اول</label>
+                        <select
+                                id="searchInput1" class="form-control select2"
+                                style="width: 20%;display: inline-block;margin-bottom: 8px">
+                            <option value=""  selected>بدون فیلتر</option>
+                            <?php
+                            $groups = mysqli_query($connection_maghalat, "select * from scientific_group order by name asc");
+                            foreach ($groups as $Group) {
+                                ?>
+                                <option value="<?php echo $Group['name']?>"><?php echo $Group['name'];?></option>
+                            <?php } ?>
+                        </select>
+                        <label>گروه علمی دوم</label>
+                        <select
+                                id="searchInput2" class="form-control select2"
+                                style="width: 20%;display: inline-block;margin-bottom: 8px">
+                            <option value=""  selected>بدون فیلتر</option>
+                            <?php
+                            $groups = mysqli_query($connection_maghalat, "select * from scientific_group order by name asc");
+                            foreach ($groups as $Group) {
+                                ?>
+                                <option value="<?php echo $Group['name']?>"><?php echo $Group['name'];?></option>
+                            <?php } ?>
+                        </select>
+                        <button class="btn btn-primary" onclick="searchTable()">فیلتر کردن</button>
+
+                        <script>
+                            function searchTable() {
+                                var input1, input2, filter1, filter2, table, tr, td1, td2, i;
+                                input1 = document.getElementById("searchInput1");
+                                input2 = document.getElementById("searchInput2");
+                                filter1 = input1.value.toUpperCase();
+                                filter2 = input2.value.toUpperCase();
+                                table = document.querySelector("table");
+                                tr = table.querySelectorAll("tbody tr");
+
+                                for (i = 0; i < tr.length; i++) {
+                                    td1 = tr[i].getElementsByTagName("td")[3];
+                                    td2 = tr[i].getElementsByTagName("td")[4];
+                                    if (td1 && td2) {
+                                        var showRow = true;
+
+                                        if (filter1 !== "" && td1.textContent.toUpperCase().indexOf(filter1) === -1) {
+                                            showRow = false;
+                                        }
+
+                                        if (filter2 !== "" && td2.textContent.toUpperCase().indexOf(filter2) === -1) {
+                                            showRow = false;
+                                        }
+
+                                        if (filter1 === "" && filter2 === "") {
+                                            showRow = true;
+                                        }
+
+                                        if (showRow) {
+                                            tr[i].style.display = "";
+                                        } else {
+                                            tr[i].style.display = "none";
+                                        }
+                                    }
+                                }
+                            }
+
+                        </script>
+
+                    </div>
+
                     <table class="table table-bordered table-striped display" id="report_from_rates">
                         <thead>
                         <tr style="text-align: center">
