dfc5a009674cd6504b5159e508215d98253ff77d
Mohammad <exploiter0253@gmail.com>
2024-01-02 11:21:37 +0330

    Fix URL's (remove .php)

--- menus.php
+++ menus.php
@@ -7,46 +7,48 @@
         $query = mysqli_query($connection_maghalat, "select * from menus where access like '%$type%' order by level");
         foreach ($query as $menus) {
             if ($menus['Is_Parent'] == 0) {
-                ?>
+        ?>
                 <li class="nav-item">
-                    <a href="<?php echo $menus['link'] ?>"
-                       class="nav-link <?php $self = explode('/', $_SERVER['PHP_SELF']);
-                       $self = end($self);
-                       if ($self == $menus['link']) {
-                           echo 'active';
-                       } ?>">
+                    <a href="<?php echo $menus['link'] ?>" class="nav-link <?php $self = explode('/', $_SERVER['PHP_SELF']);
+                                                                            $self = end($self);
+                                                                            $self = str_replace('.php', '', $self);
+                                                                            if ($self == $menus['link']) {
+                                                                                echo 'active';
+                                                                            } ?>">
                         <i class="fa <?php echo $menus['icon'] ?> nav-icon"></i>
                         <p><?php echo $menus['subject'] ?></p>
                     </a>
                 </li>
-                <?php
+            <?php
             } elseif ($menus['Is_Parent'] == 1 and $menus['childs'] != null and $menus['childs'] != '') {
                 $childs = explode('|', $menus['childs']);
-                ?>
+            ?>
                 <li class="nav-item has-treeview <?php
-                foreach ($childs as $child) {
-                    $query = mysqli_query($connection_maghalat, "Select * from menus where id='$child' order by level");
-                    foreach ($query as $list) {
-                    }
-                    $self = explode('/', $_SERVER['PHP_SELF']);
-                    $self = end($self);
-                    if ($list['link'] == $self) {
-                        echo 'menu-open';
-                    }
-                }
-                ?>">
+                                                    foreach ($childs as $child) {
+                                                        $query = mysqli_query($connection_maghalat, "Select * from menus where id='$child' order by level");
+                                                        foreach ($query as $list) {
+                                                        }
+                                                        $self = explode('/', $_SERVER['PHP_SELF']);
+                                                        $self = end($self);
+                                                        $self = str_replace('.php', '', $self);
+                                                        if ($list['link'] == $self) {
+                                                            echo 'menu-open';
+                                                        }
+                                                    }
+                                                    ?>">
                     <a href="#" class="nav-link <?php
-                    foreach ($childs as $child) {
-                        $query = mysqli_query($connection_maghalat, "Select * from menus where id='$child' order by level");
-                        foreach ($query as $list) {
-                        }
-                        $self = explode('/', $_SERVER['PHP_SELF']);
-                        $self = end($self);
-                        if ($list['link'] == $self) {
-                            echo 'active';
-                        }
-                    }
-                    ?>">
+                                                foreach ($childs as $child) {
+                                                    $query = mysqli_query($connection_maghalat, "Select * from menus where id='$child' order by level");
+                                                    foreach ($query as $list) {
+                                                    }
+                                                    $self = explode('/', $_SERVER['PHP_SELF']);
+                                                    $self = end($self);
+                                                    $self = str_replace('.php', '', $self);
+                                                    if ($list['link'] == $self) {
+                                                        echo 'active';
+                                                    }
+                                                }
+                                                ?>">
                         <i class="nav-icon fa <?php echo $menus['icon']; ?>"></i>
                         <p>
                             <?php echo $menus['subject'] ?>
@@ -62,27 +64,30 @@
                             foreach ($query as $Child_Info) {
                             }
 
-                            ?>
+                        ?>
                             <li class="nav-item">
                                 <a href="<?php $self = explode('/', $_SERVER['PHP_SELF']);
-                                $self = end($self);
-                                if ($self == $Child_Info['link']) {
-                                    echo '#';
-                                } else{
-                                    echo $Child_Info['link'];
-                                }?>" class="nav-link <?php $self = explode('/', $_SERVER['PHP_SELF']);
-                                $self = end($self);
-                                if ($self == $Child_Info['link']) {
-                                    echo 'active';
-                                } ?>">
+                                            $self = end($self);
+                                            $self = str_replace('.php', '', $self);
+                                            if ($self == $Child_Info['link']) {
+                                                echo '#';
+                                            } else {
+                                                echo $Child_Info['link'];
+                                            } ?>" class="nav-link <?php $self = explode('/', $_SERVER['PHP_SELF']);
+                                                        $self = end($self);
+                                                        $self = str_replace('.php', '', $self);
+                                                        if ($self == $Child_Info['link']) {
+                                                            echo 'active';
+                                                        } ?>">
                                     <i class="fa
                                       <?php $self = explode('/', $_SERVER['PHP_SELF']);
-                                    $self = end($self);
-                                    if ($self == $Child_Info['link']) {
-                                        echo 'fa-dot-circle-o';
-                                    }else{
-                                        echo 'fa-circle-o';
-                                    }?>
+                                        $self = end($self);
+                                        $self = str_replace('.php', '', $self);
+                                        if ($self == $Child_Info['link']) {
+                                            echo 'fa-dot-circle-o';
+                                        } else {
+                                            echo 'fa-circle-o';
+                                        } ?>
                                       nav-icon"></i>
                                     <p><?php
                                         echo $Child_Info['subject'];
@@ -92,7 +97,7 @@
                         <?php } ?>
                     </ul>
                 </li>
-                <?php
+        <?php
             }
         }
         ?>
